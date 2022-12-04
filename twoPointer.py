5fd9cb54c676824aacbbb5e61ca3a864a63854fc
yonniiii <119270799+yonniiii@users.noreply.github.com>
2022-12-04 18:26:50 +0900

    Update twoPointer.py

--- twoPointer.py
+++ twoPointer.py
@@ -22,3 +22,16 @@ def checkIfPalindrome(s):
     return True
 
 print(checkIfPalindrome('ABCBA'))
+
+def check_for_target(nums,target):
+    left = 0
+    right = len(nums) -1
+    while left < right:
+        curr = nums[left] + nums[right]
+        if curr == target:
+            return True
+        if curr > target:
+            right -=1
+        else:
+            left += 1
+    return False
